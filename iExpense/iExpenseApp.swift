6bdc99c0e79074850bf0bc1abd2706432d87ca9b
Rob Downing <redowning@gmail.com>
2025-11-07 23:40:34 -0800

    Initial commit: iExpense project setup

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Rob Downing on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
